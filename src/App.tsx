02beb2e6235dcabc9922bac0daee86135bbafe47
Danish <dkj23@uclive.ac.nz>
2022-05-17 16:56:55 +1200

    Set up the project skeleton and the App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from './components/Login';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <Routes>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/HeaderNav.tsx
@@ -0,0 +1,5 @@
+const HeaderNav = () => {
+
+}
+
+export default HeaderNav;
